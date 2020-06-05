e7594a59d575967d9285f1ce15efc6fb842b5b6c
Chege Bryan <cbbryan8@gmail.com>
2020-06-05 17:51:47 +0300

    🗂  Add blog initial model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """Model representing a blog """
+    title = models.CharField(max_length=200)
+    content = models.TextField(help_text='Enter the blog content')
+    post_date = models.DateField(auto_now_add=True)
