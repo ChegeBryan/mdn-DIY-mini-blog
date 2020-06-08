3d7fa33f83ac25c65c7de2dbcc0a4d28d6994f53
Chege Bryan <cbbryan8@gmail.com>
2020-06-08 20:01:49 +0300

    ⚙ Update allowed hosts

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diyblogcb.herokuapp.com', 'localhost']
 
 
 # Application definition
