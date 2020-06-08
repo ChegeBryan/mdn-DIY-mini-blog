f2c58d4c0326ad372c5061dba0fd1bb9072b771b
Chege Bryan <cbbryan8@gmail.com>
2020-06-08 19:53:02 +0300

    ⚙ Read secret key from environment variables

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ljk8mto%92)49yuxtzz79xu!+@e#ft(wv&ud)w60_&jzqs8c9s'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'kdod#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
