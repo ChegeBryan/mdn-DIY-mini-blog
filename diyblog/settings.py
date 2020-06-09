94f390ccda5abc101db8fd3a33cb7f5ab6ed6c62
Chege Bryan <cbbryan8@gmail.com>
2020-06-09 10:57:21 +0300

    🐞 Update allowed host with right localhsot address

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['diyblogcb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['diyblogcb.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
