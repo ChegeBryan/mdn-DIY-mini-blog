6ff1400288217f30d35167931c1a4c4547f18226
Chege Bryan <cbbryan8@gmail.com>
2020-06-09 12:18:34 +0300

    🎨 Inline blog with associated comments

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from .models import Blog, Comment
 
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'post_date')
+    inlines = [CommentInline, ]
 
 
 @admin.register(Comment)
