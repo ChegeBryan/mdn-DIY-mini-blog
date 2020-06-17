70137e726f0c5c10834ea3abfb14bdc6adb10a80
Chege Bryan <cbbryan8@gmail.com>
2020-06-17 11:32:21 +0300

    🔗 Add url mapping for bloggers page

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
     path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/bloggers/', views.BloggerListView.as_view(), name='bloggers')
 ]
