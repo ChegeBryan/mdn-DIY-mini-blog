6149795324eed9d89de75ea462e548c30d609dae
Chege Bryan <cbbryan8@gmail.com>
2020-06-26 13:16:29 +0300

    📝 Add view to add comment

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
-from .models import Blog, Blogger
+from .models import Blog, Blogger, Comment
 
 
 def index(request):
@@ -26,3 +27,8 @@ class BloggerListView(ListView):
 
 class BloggerDetailView(DetailView):
     model = Blogger
+
+
+class CommentAdd(CreateView):
+    model = Comment
+    fields = ['comment']
