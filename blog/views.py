6bc48b46aa4caef8c13502d13fd3d059ed5b4f45
Chege Bryan <cbbryan8@gmail.com>
2020-06-26 14:14:05 +0300

    🎨 Add read-only comment data to validate form

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
@@ -37,3 +39,16 @@ class CommentAdd(CreateView):
         context = super().get_context_data(**kwargs)
         context["blog"] = get_object_or_404(Blog, pk=self.kwargs['pk'])
         return context
+
+    def form_valid(self, form):
+        """
+        Add author and associated blog to form data before setting it as valid (so it is saved to model)
+        """
+        # Add logged-in user as author of comment
+        form.instance.author = self.request.user
+        # Associate comment with blog based on passed id
+        form.instance.blog = get_object_or_404(Blog, pk=self.kwargs['pk'])
+        # Add current time to form
+        form.instance.post_date = datetime.datetime.now()
+        # Call super-class form validation behaviour
+        return super().form_valid(form)
